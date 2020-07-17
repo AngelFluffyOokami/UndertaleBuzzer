c72253320b4e6c6ea6335a4a8697866c5ef721d3
AngelFluffyOokami <59583625+AngelFluffyOokami@users.noreply.github.com>
2020-07-16 20:58:22 -0500

    Created friend.sh
    Added "Your Best Friend"

---
+++ friend.sh
@@ -0,0 +1,75 @@
+beep -f 415	-l 230	-D 230		#Ab4
+#
+beep -f 523     -l 230  -D 230          #C5
+#
+beep -f 311     -l 230  -D 230          #Eb4
+#
+beep -f 523     -l 230  -D 230          #C5
+#
+beep -f 415     -l 230  -D 230          #Ab4
+#
+beep -f 523     -l 230  -D 230          #C5
+#
+beep -f 311     -l 230  -D 230          #Eb4
+#
+beep -f 523     -l 230  -D 230          #C5
+#
+beep -f 415     -l 230  -D 230          #Ab4
+#
+beep -f 523     -l 230  -D 230          #C5
+#
+beep -f 311     -l 230  -D 230          #Eb4
+#
+beep -f 523     -l 230  -D 230          #C5
+#
+beep -f 415     -l 230  -D 230          #Ab4
+#
+beep -f 523     -l 230  -D 230          #C5
+#
+beep -f 311     -l 230  -D 230          #Eb4
+#
+beep -f 523     -l 230  -D 230          #C5
+
+beep -f 1046 	-l 230 	-D 230		#C6
+
+beep -f 1046 	-l 230			#C6
+beep -f 1108 	-l 230			#Db6
+beep -f 1244 	-l 460			#Eb6
+beep -f 1046 	-l 460			#C6
+beep -f 1108 	-l 230 	-D 230		#Db6
+
+beep -f 1108 	-l 230			#Db6
+beep -f 1244 	-l 230 			#Eb6
+beep -f 932 	-l 460 			#Bb5
+beep -f 622 	-l 460 			#Eb5
+beep -f 1046 	-l 230 	-D 230 		#C6
+
+beep -f 1046 	-l 230 			#C6
+beep -f 1108 	-l 230 			#Db6
+beep -f 1244 	-l 460			#Eb6
+beep -f 1661 	-l 460			#Ab6
+beep -f 1396 	-l 920			#F6
+beep -f 1244 	-l 230 	-D 230		#Eb6
+
+beep -f 2489 	-l 230 	-D 230		#Eb7
+
+beep -f 1046 	-l 230 	-D 230		#C6
+
+beep -f 1046 	-l 230			#C6
+beep -f 1108 	-l 230			#Db6
+beep -f 1244 	-l 460			#Eb6
+beep -f 1046 	-l 460			#C6
+beep -f 1108 	-l 230	-D 230		#Db6
+
+beep -f 1108 	-l 230			#Db6
+beep -f 1244	-l 230			#Eb6
+beep -f 932	-l 460			#Bb5
+beep -f 783	-l 460			#G5
+beep -f 1046	-l 230	-D 230		#C6
+
+beep -f 1046	-l 230			#C6
+beep -f 1108	-l 230			#Db6
+beep -f 932	-l 460			#Bb5
+beep -f 783	-l 460			#G5
+beep -f 830	-l 920			#Ab5
+beep -f 1661 	-l 460 	-D 460		#Ab6
