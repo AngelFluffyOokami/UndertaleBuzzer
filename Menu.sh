160f9b35a7687a33ddca5712905728c636c4c0a2
AngelFluffyOokami <59583625+AngelFluffyOokami@users.noreply.github.com>
2020-07-16 19:33:13 -0500

    Create Menu.sh
    Add Menu.sh

---
+++ Menu.sh
@@ -0,0 +1,31 @@
+beep -f 783 -l 170 -D 510
+beep -f  1567 -l 170 -D 340
+beep -f 1174 -l 170 -D 1360
+
+beep -f 1046 -l 170 -D 510
+beep -f 1567 -l 170 -D 340
+beep -f 783 -l 170 -D 1360
+
+beep -f 783 -l 170 -D 510
+beep -f 1046 -l 170 -D 340
+beep -f 1567 -l 170 -D 680
+beep -f 1760 -l 170 -D 510
+beep -f 1567 -l 170 -D 510
+beep -f 1174 -l 170 -D 340
+beep -f 1046 -l 170 -D 1360
+
+beep -f 783 -l 170 -D 510
+beep -f  1567 -l 170 -D 340
+beep -f 1174 -l 170 -D 1360
+
+beep -f 1046 -l 170 -D 510
+beep -f 1567 -l 170 -D 340
+beep -f 783 -l 170 -D 1360
+
+beep -f 783 -l 170 -D 510
+beep -f 1046 -l 170 -D 340
+beep -f 1567 -l 170 -D 680
+beep -f 1760 -l 170 -D 510
+beep -f 1567 -l 170 -D 510
+beep -f 1174 -l 170 -D 340
+beep -f 1046 -l 170 -D 1360
