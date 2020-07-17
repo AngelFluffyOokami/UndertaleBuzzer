8df79e057f13cf722c5c1f27525761e42d3b6ea0
AngelFluffyOokami <59583625+AngelFluffyOokami@users.noreply.github.com>
2020-07-16 23:12:36 -0500

    Created onceupon.sh
    Added "Once Upon a Time"

---
+++ onceupon.sh
@@ -0,0 +1,145 @@
+beep -f 523	-l 900			#C5
+beep -f 1046	-l 900			#C6
+beep -f 783	-l 1800			#G5
+beep -f 698 	-l 900			#F5
+beep -f 1046 	-l 900			#C6
+beep -f 523	-l 1800	-D 50		#C5
+beep -f 523	-l 900			#C5
+beep -f 698	-l 900			#F5
+beep -f 1046	-l 900			#C6
+beep -f 1174	-l 900			#D6
+beep -f 1046	-l 900			#C6
+beep -f 783	-l 900			#G5
+beep -f 698	-l 1800			#F5
+beep -f 523	-l 900			#C5
+beep -f 1046	-l 900			#C6
+beep -f 783	-l 1800			#G5
+beep -f 698	-l 900			#F5
+beep -f 1046	-l 900			#C6
+beep -f 523	-l 1800	-D 50		#C5
+beep -f 523	-l 900			#C5
+beep -f 698	-l 900			#F5
+beep -f 1046	-l 900			#C6
+beep -f 1174	-l 900			#D6
+beep -f 1046	-l 900			#C6
+beep -f 783	-l 900			#G5
+beep -f 698	-l 1800			#F5
+beep -f 261	-l 900			#C4
+beep -f 523     -l 300  -D 150          #C5
+#
+beep -f 440     -l 300  -D 150          #A4
+#
+beep -f 392     -l 400                  #G4
+beep -f 349     -l 400                  #F4
+beep -f 329     -l 400                  #E4
+beep -f 349     -l 400                  #F4
+beep -f 392     -l 900                  #G4
+beep -f 659	-l 900			#E5
+beep -f 523     -l 1800                 #C5
+beep -f 293     -l 900                  #D4
+beep -f 523     -l 600  -D 200          #C5
+#
+beep -f 440     -l 600  -D 200          #A4
+#
+beep -f 392     -l 400                  #G4
+beep -f 349     -l 400                  #F4
+beep -f 392     -l 400                  #G4
+beep -f 440     -l 400                  #A4
+beep -f 329     -l 900                  #E4
+beep -f 392     -l 900  -D 50           #G4
+beep -f 392     -l 400                  #G4
+beep -f 349     -l 400                  #F4
+beep -f 329     -l 400                  #E4
+beep -f 349     -l 400                  #F4
+beep -f 261     -l 900                  #C4
+beep -f 523     -l 600  -D 200          #C5
+#
+beep -f 440     -l 600  -D 200          #A4
+#
+beep -f 392     -l 400                  #G4
+beep -f 349     -l 400                  #F4
+beep -f 329     -l 400                  #E4
+beep -f 349     -l 400                  #F4
+beep -f 392     -l 900                  #G4
+beep -f 659     -l 900                  #E5
+beep -f 523     -l 1800                 #C5
+beep -f 298     -l 900                  #D4
+beep -f 523     -l 600  -D 200          #C5
+#
+beep -f 440     -l 600  -D 200          #A4
+#
+beep -f 392     -l 400                  #G4
+beep -f 349     -l 400                  #F4
+beep -f 392     -l 400                  #G4
+beep -f 440     -l 400                  #A4
+beep -f 329     -l 900                  #E4
+beep -f 392     -l 900  -D 50           #G4
+beep -f 392     -l 400                  #G4
+beep -f 349     -l 400                  #F4
+beep -f 329     -l 400                  #E4
+beep -f 349     -l 400                  #F4
+##
+beep -f 1046    -l 400                  #C6
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 493     -l 400                  #B4
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 466     -l 400                  #Bb4
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 932     -l 400                  #Bb5
+beep -f 880     -l 400                  #A5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 523     -l 400                  #C5
+beep -f 1046    -l 400                  #C6
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 493     -l 400                  #B4
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 466     -l 400                  #Bb4
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 932     -l 400                  #Bb5
+beep -f 880     -l 400                  #A5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 523     -l 400                  #C5
+beep -f 1046    -l 400                  #C6
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 493     -l 400                  #B4
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 466     -l 400                  #Bb4
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 932     -l 400                  #Bb5
+beep -f 880     -l 400                  #A5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 523     -l 400                  #C5
+beep -f 1046    -l 400                  #C6
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 493     -l 400                  #B4
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 466     -l 400                  #Bb4
+beep -f 698     -l 400                  #F5
+beep -f 783     -l 400                  #G5
+beep -f 932     -l 400                  #Bb5
+beep -f 880     -l 400                  #A5
+beep -f 783     -l 400                  #G5
+beep -f 698     -l 400                  #F5
+beep -f 523     -l 900                  #C5
