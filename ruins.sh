fc4d070efb4d5b48af4f256b385201c2e7625eed
AngelFluffyOokami <59583625+AngelFluffyOokami@users.noreply.github.com>
2020-07-17 21:14:58 -0500

    Created ruins.sh
    Added ruins.sh

---
+++ ruins.sh
@@ -0,0 +1,61 @@
+beep -f 415	-l 200				#Ab4
+beep -f 622	-l 200				#Eb5
+beep -f 493	-l 400				#B4
+beep -f 466	-l 200				#Bb4
+beep -f 493	-l 200				#B4
+beep -f 554	-l 250	-D 1000			#Db4
+#
+beep -f 369	-l 200				#Gb4
+beep -f 415	-l 200				#Ab4
+beep -f 622	-l 200				#Eb5
+beep -f 493     -l 400				#B4
+beep -f 466	-l 200				#Bb4
+beep -f 493     -l 200                          #B4
+beep -f 554     -l 250  -D 100	                #Db4
+#
+beep -f 1396	-l 250				#F6
+beep -f 1479	-l 50	-D 70			#Gb6
+beep -f 1396	-l 50				#F6
+beep -f 1479	-l 50				#Gb6
+beep -f 1396	-l 130				#F6
+beep -f 1108	-l 200				#Db6
+beep -f 1244	-l 550	-D 100			#Eb6
+#
+beep -f 1975	-l 200				#B6
+beep -f 1661	-l 170	-D 150			#Ab6
+#
+beep -f 1864	-l 200				#Bb6
+beep -f 1975	-l 200				#B6
+beep -f 2227	-l 200				#Db7
+beep -f 1864    -l 200                          #Bb6
+beep -f 1975    -l 400  -D 200                  #B6
+#
+beep -f 1864	-l 150	-D 200			#Bb6
+#
+beep -f 1661	-l 100				#Ab6
+beep -f 1479	-l 150				#Gb6
+beep -f 1396	-l 100	-D 300			#F6
+#
+beep -f 1975	-l 200				#B6
+beep -f 2217	-l 200				#Db6
+beep -f 2489	-l 200				#Eb7
+beep -f 2793	-l 200				#F7
+beep -f 1975    -l 900                          #B6
+beep -f 1864	-l 50	-D 300			#Bb6
+#
+beep -f 1244	-l 200				#Eb6
+beep -f 1661	-l 400				#Ab6
+beep -f 1479	-l 200	-D 25			#Gb6
+beep -f 1396	-l 100	-D 200			#F6
+#
+beep -f 1479	-l 150				#Gb6
+beep -f 1396	-l 200				#F6
+beep -f 1244	-l 200				#Eb6
+beep -f 1108	-l 400	-D 50			#Db6
+beep -f 1661	-l 200				#Ab6
+beep -f 1244	-l 200	-D 400			#Eb6
+#
+beep -f 932	-l 200				#Bb5
+beep -f 987	-l 200				#B5
+beep -f 987	-l 200				#Bb5
+beep -f 1108	-l 1400
