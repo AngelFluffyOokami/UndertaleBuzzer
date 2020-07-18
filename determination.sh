fc4a267076097501f1f647e7588d14b90da87c2d
AngelFluffyOokami <59583625+AngelFluffyOokami@users.noreply.github.com>
2020-07-18 15:50:28 -0500

    Created determination.sh
    Added determination.sh

---
+++ determination.sh
@@ -0,0 +1,158 @@
+beep -f 1396	-l 250				#F6
+beep -f 1318	-l 250				#E6
+beep -f 1174	-l 250				#D6
+beep -f 1046	-l 250				#C6
+beep -f 1174	-l 250				#D6
+beep -f 880	-l 250				#A5
+beep -f 987	-l 250	-D 250			#B5
+#
+beep -f 783	-l 250	-D 250			#G5
+#
+beep -f 1174    -l 250                          #D6
+beep -f 1318    -l 250                          #E6
+beep -f 1396    -l 250  -D 250                  #F6
+#
+beep -f 1567	-l 250	-D 250			#G6
+#
+beep -f 2093	-l 250	-D 250			#C7
+#
+beep -f 1760	-l 1375	-D 50			#A7
+#
+beep -f 1396    -l 250		                #F6
+beep -f 1318    -l 250                          #E6
+beep -f 1174    -l 250                          #D6
+beep -f 1046    -l 250                          #C6
+beep -f 1174    -l 250                          #D6
+beep -f 880     -l 250                          #A5
+beep -f 987     -l 250  -D 250                  #B5
+#
+beep -f 783     -l 250  -D 250                  #G5
+#
+beep -f 1174    -l 250                          #D6
+beep -f 1318    -l 250                          #E6
+beep -f 1396    -l 250  -D 250                  #F6
+#
+beep -f 1318    -l 250  -D 250                  #E6
+#
+beep -f 1046    -l 250  -D 250                  #C6
+#
+beep -f 1174    -l 1375 -D 50                   #D6
+#
+beep -f 1396    -l 250                          #F6
+beep -f 1318    -l 250                          #E6
+beep -f 1174    -l 250                          #D6
+beep -f 1046    -l 250                          #C6
+beep -f 1174    -l 250                          #D6
+beep -f 880     -l 250                          #A5
+beep -f 987     -l 250  -D 250                  #B5
+#
+beep -f 783     -l 250  -D 250                  #G5
+#
+beep -f 1174    -l 250                          #D6
+beep -f 1318    -l 250                          #E6
+beep -f 1396    -l 250  -D 250                  #F6
+#
+beep -f 1567    -l 250  -D 250                  #G6
+#
+beep -f 2093    -l 250  -D 250                  #C7
+#
+beep -f 1760    -l 1375 -D 50                   #A7
+#
+beep -f 1396    -l 250                          #F6
+beep -f 1318    -l 250                          #E6
+beep -f 1174    -l 250                          #D6
+beep -f 1046    -l 250                          #C6
+beep -f 1174    -l 250                          #D6
+beep -f 880     -l 250                          #A5
+beep -f 987     -l 250  -D 250                  #B5
+#
+beep -f 783     -l 250  -D 250                  #G5
+#
+beep -f 1174    -l 250                          #D6
+beep -f 1318    -l 250                          #E6
+beep -f 1396    -l 250  -D 250                  #F6
+#
+beep -f 1318    -l 250  -D 250                  #E6
+#
+beep -f 1046    -l 250  -D 250                  #C6
+#
+beep -f 1174    -l 1375 -D 50                   #D6
+#
+beep -f 3135	-l 250				#G7
+beep -f 2793	-l 250				#F7
+beep -f 2489	-l 250				#Eb7
+beep -f 2349	-l 250				#D7
+beep -f 2093    -l 250  			#C7
+beep -f 2489    -l 250                          #Eb7
+beep -f 2349    -l 250  -D 250                  #D7
+#
+beep -f 1760	-l 250	-D 250			#A7
+#
+beep -f 1760    -l 250                          #A7
+beep -f 2349    -l 250                          #D7
+beep -f 3135    -l 250                          #G7
+beep -f 2793    -l 250                          #F7
+beep -f 2489    -l 250                          #Eb7
+beep -f 2349    -l 250                          #D7
+beep -f 2093    -l 250                          #C7
+beep -f 2489    -l 250                          #Eb7
+beep -f 2349    -l 1375                         #D7
+beep -f 1174    -l 250                          #D6
+beep -f 1567	-l 250				#G6
+beep -f 2093    -l 250                          #C7
+beep -f 1975	-l 250				#B6
+beep -f 1760	-l 250				#A6
+beep -f 1567	-l 250				#G6
+beep -f 1760    -l 250                          #A6
+beep -f 1975    -l 250                          #B6
+beep -f 1760    -l 250  -D 250	                #A6
+#
+beep -f 1174    -l 250  -D 250                  #D6
+#
+beep -f 1174    -l 250                          #D6
+beep -f 1318	-l 250				#E6
+beep -f 1396	-l 250	-D 250			#F6
+#
+beep -f 1760    -l 250  -D 250                  #A6
+#
+beep -f 2349    -l 500                          #D7
+beep -f 2217	-l 1375				#Db7
+beep -f 3135    -l 250                          #G7
+beep -f 2793    -l 250                          #F7
+beep -f 2489    -l 250                          #Eb7
+beep -f 2349    -l 250                          #D7
+beep -f 2093    -l 250                          #C7
+beep -f 2489    -l 250                          #Eb7
+beep -f 2349    -l 250  -D 250                  #D7
+#
+beep -f 1760    -l 250  -D 250                  #A6
+#
+beep -f 1760    -l 250                          #A6
+beep -f 2349    -l 250                          #D7
+beep -f 3135    -l 250                          #G7
+beep -f 2793    -l 250                          #F7
+beep -f 2489    -l 250                          #Eb7
+beep -f 2349    -l 250                          #D7
+beep -f 2093    -l 250                          #C7
+beep -f 2489    -l 250                          #Eb7
+beep -f 2349    -l 1375                         #D7
+beep -f 1174    -l 250                          #D6
+beep -f 1567    -l 250                          #G6
+beep -f 2093    -l 250                          #C7
+beep -f 1975    -l 250                          #B6
+beep -f 1760    -l 250                          #A6
+beep -f 1567    -l 250                          #G6
+beep -f 1760    -l 250                          #A6
+beep -f 1975    -l 250                          #B6
+beep -f 1760    -l 250  -D 250                  #A6
+#
+beep -f 1174    -l 250  -D 250                  #D6
+#
+beep -f 1174    -l 250                          #D6
+beep -f 1318    -l 250                          #E6
+beep -f 1396    -l 250  -D 250                  #F6
+#
+beep -f 1318    -l 250	-D 250                  #E6
+#
+beep -f 1046	-l 500				#C6
+beep -f 1174	-l 1375				#D6
